c39b8416d237cbb4731b770d66935fbe55bb5c7e
James Fitzgerald <nimblewillcreek@gmail.com>
2015-07-31 12:31:50 -0400

    Create geojson.js

---
+++ javascripts/geojson.js
@@ -0,0 +1,47 @@
+/**
+ * @author fitzgj028555
+ */
+
+var Career = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+      	 "popupContent": "University of North Georgia: IESA"      	 
+      },          
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -83.86535882949829,
+          34.234760721381456
+        ]
+       }
+      },
+    {
+      "type": "Feature",
+      "properties": {
+      	"popupContent":"Appalachian Soil, INC"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.52194213867188,
+          34.636456464420995
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"popupContent":"Cobb County tax Assessor's Office"},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.5738697052002,
+          33.95329236373766
+        ]
+      }
+    }
+  ]
+};
