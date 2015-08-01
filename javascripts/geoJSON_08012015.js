a6244d5ae4d0856badef5b882255f48fd7c985c2
James Fitzgerald <nimblewillcreek@gmail.com>
2015-08-01 14:58:17 -0400

    Create geoJSON_08012015.js

---
+++ javascripts/geoJSON_08012015.js
@@ -0,0 +1,16 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.57404136657715,
+          33.95368394066259
+        ]
+      }
+    }
+  ]
+}
