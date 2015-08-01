ea66b219387be2b976a13a04931e1107144aa073
James Fitzgerald <nimblewillcreek@gmail.com>
2015-08-01 16:14:12 -0400

    Update geojson.js

--- javascripts/geojson.js
+++ javascripts/geojson.js
@@ -7,7 +7,7 @@
     {
       "type": "Feature",
       "properties": {
-        "popupContent2": "Tax Assessor's Office"
+        "popupContent": "Tax Assessor's Office"
       },
       "geometry": {
         "type": "Point",
@@ -19,24 +19,4 @@
     }
   ]
 };
-
-var IESA = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        "popupContent": "IESA" 
-      },          
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -83.86535882949829,
-          34.234760721381456
-        ]
-      }
-    }
-  ]
-};
-
 
