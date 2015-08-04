67e07bdf419409ea485bdde85a1cb76c6382cfcd
James Fitzgerald <nimblewillcreek@gmail.com>
2015-08-04 08:49:57 -0400

    Create Work.js

---
+++ javascripts/Work.js
@@ -0,0 +1,27 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.57389116287231,
+          33.95357714713449
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.52155590057373,
+          34.63733920141577
+        ]
+      }
+    }
+  ]
+}
