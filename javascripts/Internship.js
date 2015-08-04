2da3be7c2f773423195546fa39dd11aa5e531482
James Fitzgerald <nimblewillcreek@gmail.com>
2015-08-04 09:02:50 -0400

    Create Internship.js

---
+++ javascripts/Internship.js
@@ -0,0 +1,27 @@
+var internship = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -83.81442368030548,
+          34.33697798269263
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -83.79826605319977,
+          33.62805165741625
+        ]
+      }
+    }
+  ]
+}
