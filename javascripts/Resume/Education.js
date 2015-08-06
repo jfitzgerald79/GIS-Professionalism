01c86b4f4449e7a2b7fb84a08baac666c7bce1e9
James Fitzgerald <nimblewillcreek@gmail.com>
2015-08-06 16:09:41 -0400

    Create Education.js

---
+++ javascripts/Resume/Education.js
@@ -0,0 +1,31 @@
+var College = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+                "Title": "Interactive Resume",
+                "Position": "Student-Lewis F. Rogers Institute for Environmental & Spatial Analysis",
+                "Duties": "Complete a Bachelor of Science in Environmental Spatial Analysis, and apply GIS course work to class projects"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -83.86563777923583,
+          34.23515099834814
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -83.37243318557739,
+          33.95375513627352
+        ]
+      }
+    }
+  ]
+}
