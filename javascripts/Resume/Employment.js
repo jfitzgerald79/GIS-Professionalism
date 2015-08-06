2df75cee7ca6f79c8c0a90ce14cea2b90313d8fe
James Fitzgerald <nimblewillcreek@gmail.com>
2015-08-06 16:41:10 -0400

    Update Employment.js

--- javascripts/Resume/Employment.js
+++ javascripts/Resume/Employment.js
@@ -8,7 +8,8 @@ var work = {
                 "Employer":"Cobb County Tax Assessor's"
                 "Position":"GIS Specialist I",
                 "Duties":"Planning and executing complex projects, special-purpose and general-use maps, learn and implement new methods to acquire data and present data",
-                "DOE":"July 23, 2012"
+                "DOE":"July 23, 2012",
+                "Number":"2"
       },
       "geometry": {
         "type": "Point",
@@ -25,7 +26,8 @@ var work = {
               "Employer":"Appalachian Soil, INC"
               "Position":"Soil Technician",
               "Duties":"Analyze soil content for Level I-III soil reports under direct supervision, and relate geology, hydrology, and soils to determine best placement of absorption area and septic systems",
-              "DOE":"April 4, 2006"
+              "DOE":"April 4, 2006",
+              "Number":"2"
       },
       "geometry": {
         "type": "Point",
