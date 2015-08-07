ff5822c9ab2f3c696409f852cc99627a18c57e87
James Fitzgerald <nimblewillcreek@gmail.com>
2015-08-07 16:20:53 -0400

    Update Intership.js

--- javascripts/Resume/Intership.js
+++ javascripts/Resume/Intership.js
@@ -1,1 +1,45 @@
-
+var Internship = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+                "Legend":"Internship",
+                "Title": "Interactive Resume",
+                "Organization":"USDA Forest Service",
+                "Position": "GIS Internship",
+                "Duties": "Complete a Bachelor of Science in Environmental Spatial Analysis",
+                "Date":"May 3, 2012"
+                
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -83.81447196006775,
+          34.336995701029394
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+                "Legend":"Internship",
+                "Title": "Interactive Resume",
+                "Employer":"University of North Georgia",
+                "Position": "Student-Lewis F. Rogers Institute for Environmental & Spatial Analysis",
+                "Degree": "Complete a Bachelor of Science in Environmental Spatial Analysis",
+                "DOE":"May 3, 2012",
+                "Number":"1"
+        
+      
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -83.79828214645386,
+          33.627895323342585
+        ]
+      }
+    }
+  ]
+}
