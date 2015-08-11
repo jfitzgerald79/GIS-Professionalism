07e3ffdb992da547e7cec83963e066bd7c256680
James Fitzgerald <nimblewillcreek@gmail.com>
2015-08-11 13:37:47 -0400

    Update Conferences.js

--- javascripts/Places/Conferences.js
+++ javascripts/Places/Conferences.js
@@ -1,1 +1,100 @@
-something
+var Conferences = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+                "Legend":"Conferences",
+                "Title": "ESRI International UC",
+                "Organization":"ESRI",
+                "Position": "Map Gallery:A Three Part Suitability Analysis for Cobb County: Based on a Crime Incident Analysis, Residential Property Analysis, and Cell Tower Location Analysis",
+                "Duties": "Where do I live because I know nothing of these neighborhoods? This question can be solved by using a Geographic Information System (GIS).  By using a GIS, one can create a suitability analysis to show desirable locations for new residents. It can be simple or very complex.",
+                "Dates":"July 20-24, 2015"
+                
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -117.161407,
+          32.705185
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+                "Legend":"Conferences",
+                "Title": "Georgia Geospatial Conference 2014",
+                "Organization":"Georgia URISA, ASPRS Mid South, SAMSOG, and GITA",
+                "Position": "Residential Property Sale Analysis for 2013 and 2014",
+                "Duties": "Analysis shows where residential properties experienced a higher number of 2013 sales than 2014. Home buyers found lower values in 2013 than 2014.",
+                "Dates":"May 2015"
+                
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -83.37243318557739,
+          33.95375513627352
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+                "Legend":"Map Gallery",
+                "Title": "Atlanta Regional Commission(ARC)",
+                "Organization":"The Atlanta Regional Commission is designated as a Metropolitan Area Planning and Development Commission",
+                "Position": "Anthropology",
+                "Duties": "Bachelor of Arts and Sciences in Archaeology",
+                "Dates":"October 6-8, 2014"
+                
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.384455,
+          33.754995
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+                "Legend":"Conferences",
+                "Title": "Georgia Geospatial Conference 2014",
+                "Organization":"Georgia URISA, ASPRS Mid South, SAMSOG, and GITA",
+                "Position": "Anthropology",
+                "Duties": "Bachelor of Arts and Sciences in Archaeology",
+                "Dates":"October 6-8, 2014"
+                
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -83.37243318557739,
+          33.95375513627352
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+                "Legend":"Conferences",
+                "Title": "Georgia Geospatial Conference 2014",
+                "Organization":"Georgia URISA, ASPRS Mid South, SAMSOG, and GITA",
+                "Position": "Anthropology",
+                "Duties": "Bachelor of Arts and Sciences in Archaeology",
+                "Dates":"October 6-8, 2014"
+                
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -83.37243318557739,
+          33.95375513627352
+        ]
+      }
+    }
+  ]
+}
