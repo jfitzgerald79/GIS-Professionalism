9ab51be1cca9e9dae3783475ccd32f57aae17d4c
James Fitzgerald <nimblewillcreek@gmail.com>
2015-08-18 08:57:55 -0400

    Update Leaflet.TileLegend.js

--- javascripts/Leaflet.TileLegend.js
+++ javascripts/Leaflet.TileLegend.js
@@ -75,7 +75,7 @@ L.TileLegend = L.Class.extend({
                 this.fire('open');
             }
         }, this);
-        L.DomEvent.on(title,  function () {
+        L.DomEvent.on(title, 'click',  function () {
             toggle();
         });
         if (section.expend || this._data.expendAll) {
